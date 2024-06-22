f0ac7a57a0b90417f06ba3e28472356645a9f5c9
LahiruOshara <oshara.16@cse.mrt.ac.lk>
2024-06-22 22:50:37 +0100

    initial

---
+++ src/main/java/dev/oshara/jwt_demo/JwtDemoApplication.java
@@ -0,0 +1,16 @@
+package dev.oshara.jwt_demo;
+
+import dev.oshara.jwt_demo.config.RsaKeyProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
+public class JwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dev/oshara/jwt_demo/controller/AuthController.java
@@ -0,0 +1,27 @@
+package dev.oshara.jwt_demo.controller;
+
+import dev.oshara.jwt_demo.service.TokenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+    private static final Logger LOG = LoggerFactory.getLogger(AuthController.class);
+
+    private final TokenService tokenService;
+
+    public AuthController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/token")
+    public String token(Authentication authentication) {
+        LOG.info("Token requested for user: '{}'", authentication.getName());
+        String token = tokenService.generateToken(authentication);
+        LOG.info("Token granted: {}", token);
+        return token;
+    }
+}
